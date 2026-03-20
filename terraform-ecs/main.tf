81c1128293c5e7d2a01174b94f0984455ef52495
elveli <elveli@gmail.com>
2026-03-20 09:57:08 -0700

    feat(ecs): Add autoscaling to ECS service
    Implement target tracking autoscaling for the ECS service based on CPU utilization. This will automatically adjust the desired task count to maintain optimal performance and cost efficiency.
    
    Added lifecycle ignore for `desired_count` to prevent Terraform from interfering with autoscaling.

--- terraform-ecs/main.tf
+++ terraform-ecs/main.tf
@@ -183,4 +183,33 @@ resource "aws_ecs_service" "this" {
     container_name   = "cool-app"
     container_port   = 80
   }
+
+  lifecycle {
+    ignore_changes = [desired_count]
+  }
+}
+
+resource "aws_appautoscaling_target" "ecs_target" {
+  max_capacity       = 10
+  min_capacity       = 2
+  resource_id        = "service/${aws_ecs_cluster.this.name}/${aws_ecs_service.this.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "ecs_policy_cpu" {
+  name               = "${var.app_name}-cpu-autoscaling"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+    target_value       = 70.0
+    scale_in_cooldown  = 300
+    scale_out_cooldown = 60
+  }
 }
